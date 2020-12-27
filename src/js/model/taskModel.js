1601082792c696be9f5bf2a715420a814a072212
Jory <jory.seguin@gmail.com>
2020-12-27 12:39:56 -0500

    eslint and prettier stuff

--- src/js/model/taskModel.js
+++ src/js/model/taskModel.js
@@ -1,32 +1,28 @@
-import {Priority} from "../priority.js";
-
 /** Entity class representing a task */
 export class TaskModel {
+  static lastKey = 0;
 
-    static lastKey = 0;
+  title;
+  description;
+  dueDate;
+  priority;
+  key;
 
-    title;
-    description;
-    dueDate;
-    priority;
-    key;
+  /**
+   * Create a task object
+   * @param {string} title - the title of the task
+   * @param {string} description - brief description of the task (optional)
+   * @param {Date} dueDate - the due date of the task (optional)
+   * @param {import("./../priority").default} priority - the priority of the taks (optional)
+   */
+  constructor({ title, description = null, dueDate = null, priority = null }) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
 
-    /**
-     * Create a task object
-     * @param {string} title - the title of the task
-     * @param {string} description - brief description of the task (optional)
-     * @param {Date} dueDate - the due date of the task (optional)
-     * @param {Priority} priority - the priority of the taks (optional)
-     */
-    constructor({title, description = null, dueDate = null, priority = null}) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
+    this.done = false;
 
-        this.done = false;
-
-        this.key = this.key = ++TaskModel.lastKey;;
-    }
-    
+    this.key = this.key = ++TaskModel.lastKey;
+  }
 }
